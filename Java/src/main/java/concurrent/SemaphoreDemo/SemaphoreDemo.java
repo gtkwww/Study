6ae63107fba4e26aa97c2f8b76b7253b3e4111d2
gerenbian <gerenbian@hsgene.com>
2017-09-15 14:23:08 +0800

    去除多余引用

--- Java/src/main/java/concurrent/SemaphoreDemo/SemaphoreDemo.java
+++ Java/src/main/java/concurrent/SemaphoreDemo/SemaphoreDemo.java
@@ -1,6 +1,5 @@
 package concurrent.SemaphoreDemo;
 
-import javax.naming.Name;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
